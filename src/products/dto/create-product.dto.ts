075d40b6acf88ba9f6c6a3ea77e595d3d713c54a
Enoch Osarenren <osarenrenenoch@gmail.com>
2024-05-12 18:12:05 +0100

    added validation pipes

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,6 +1,19 @@
+import { IsInt, IsNotEmpty, IsNumber, IsNumberString, IsString, IsUUID } from "class-validator"
+
 export class CreateProductDTO {
+    @IsUUID()
+    @IsNotEmpty()
     readonly id: string
+
+    @IsNotEmpty()
+    @IsString()
     readonly name: string
+
+    @IsNotEmpty()
+    @IsInt()
     readonly qty: number
+
+    @IsNotEmpty()
+    @IsNumberString()
     readonly price: number
 }
