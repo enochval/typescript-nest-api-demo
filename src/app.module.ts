64877ab39289ed8bf8de7de7b90bc114fef55f3a
Enoch Osarenren <osarenrenenoch@gmail.com>
2024-05-12 18:59:56 +0100

    added http exception filters

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,22 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
+import { LoggerMiddleware } from './common/middlewares/logger/logger.middleware';
+import { ProductsController } from './products/products.controller';
 
 @Module({
   imports: [ProductsModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    // consumer.apply(LoggerMiddleware).forRoutes(ProductsController)
+    // consumer.apply(LoggerMiddleware).forRoutes({ path: 'products', method: RequestMethod.GET })
+    consumer
+      .apply(LoggerMiddleware)
+      .exclude({ path: 'products', method: RequestMethod.GET })
+      .forRoutes(ProductsController)
+  }
+}
--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,9 +1,11 @@
-import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Delete, Get, HttpException, HttpStatus, Param, Post, UseFilters } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { CreateProductDTO } from './dto/create-product.dto';
 import { Product } from './interfaces/product.interface';
+import { HttpExceptionFilter } from 'src/common/filters/http-exception.filter';
 
 @Controller('products')
+@UseFilters(HttpExceptionFilter)
 export class ProductsController {
 
     constructor(private productService: ProductsService) {}
@@ -20,7 +22,13 @@ export class ProductsController {
 
     @Get(':id')
     async findOne(@Param() params): Promise<Product> {
-        return this.productService.findOne(params.id);
+        const product = this.productService.findOne(params.id);
+
+        if (!product) {
+            throw new HttpException("Product not found", HttpStatus.BAD_REQUEST);
+        }
+
+        return product;
     }
 
     @Delete(':id')
