4acfcd7cc63ea02fe0f631694592427738c50001
Enoch Osarenren <osarenrenenoch@gmail.com>
2024-05-13 08:29:10 +0100

    added interceptors

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,11 +1,24 @@
-import { BadRequestException, Body, Controller, Delete, Get, HttpException, HttpStatus, Param, Post, UseFilters } from '@nestjs/common';
+import { 
+    Body, 
+    Controller, 
+    Delete, 
+    Get, 
+    HttpException, 
+    HttpStatus, 
+    Param, 
+    Post, 
+    UseFilters, 
+    UseInterceptors 
+} from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { CreateProductDTO } from './dto/create-product.dto';
 import { Product } from './interfaces/product.interface';
 import { HttpExceptionFilter } from 'src/common/filters/http-exception.filter';
+import { TransformInterceptor } from 'src/common/interceptors/transform/transform.interceptor';
 
 @Controller('products')
 @UseFilters(HttpExceptionFilter)
+@UseInterceptors(TransformInterceptor)
 export class ProductsController {
 
     constructor(private productService: ProductsService) {}
@@ -22,7 +35,7 @@ export class ProductsController {
 
     @Get(':id')
     async findOne(@Param() params): Promise<Product> {
-        const product = this.productService.findOne(params.id);
+        const product = this.productService.findOne(para
 
         if (!product) {
             throw new HttpException("Product not found", HttpStatus.BAD_REQUEST);
